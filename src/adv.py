f9cecc3e6f4fb404bed9622e321ae44d0d2e569f
brellin <cekakid@gmail.com>
2019-12-05 13:05:14 -0600

    add 2 new rooms

--- src/adv.py
+++ src/adv.py
@@ -5,22 +5,29 @@ from item import Item
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'outside':  Room('Outside Cave Entrance', 'North of you, the cave mount beckons'),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'foyer':    Room('Foyer', '''Dim light filters in from the south. Dusty
+passages run north and east.'''),
+
+    'overlook': Room('Grand Overlook', '''A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.'''),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room('Narrow Passage', '''The narrow passage bends here from west
+to north. The smell of gold permeates the air.'''),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room('Treasure Chamber', f'''You've found the long-lost treasure
+chamber! The only exit is to the south.'''),
+
+    'staircase': Room('Staircase', '''Stairs connecting the downstairs to the upstairs 
+of this house.Nothing interesting, just some stone stairs.
+Move north to go upstairs and south to go downstairs.'''),
+
+    'loft': Room('Loft', '''The staircase opens to a quaint room with a rail, overlooking the foyer. 
+There is a comfy-looking chair and a nightstand sitting next to a window.'''),
+
 }
 
 item_list = {
@@ -37,10 +44,13 @@ room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
+room['foyer'].w_to = room['staircase']
 room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room['staircase'].s_to = room['foyer']
+room['staircase'].n_to = room['loft']
 
 # Add items to rooms
 room['outside'].add_item_to_room(item_list['arrows'])
